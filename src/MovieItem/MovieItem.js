cbd1db24d8f41b69f0edb5c6a23db5a67bafb111
Klaudia <klaudia.krzywda@gmail.com>
2020-04-04 13:58:51 +0100

    refactoring topbar section

--- src/MovieItem/MovieItem.js
+++ src/MovieItem/MovieItem.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './MovieItem.scss';
 
-const MovieItem = () => {
-  return <div className="movieItem">MovieItem</div>;
+const MovieItem = (props) => {
+  return (
+    <li key={props.movie.id} className="movieList__item">
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/w200${props.movie.poster_path}`}
+          alt="avatar"
+        />
+      </div>
+      <p>{props.movie.title}</p>
+      <p>{props.movie.vote_average}</p>
+    </li>
+  );
 };
 
 export default MovieItem;
--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,73 +1,38 @@
 import React, { Component } from 'react';
+import MovieItem from '../MovieItem/MovieItem';
 import './MovieList.scss';
 
 class MovieList extends Component {
   render() {
     return (
       <div className="movieList">
-        <div>
-          <ul className="movieList__list">
-            {this.props.searchMovie.map((movie) => {
-              return (
-                <li key={movie.id} className="movieList__item">
-                  <div>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                      alt="avatar"
-                    />
-                  </div>
-                  <p>{movie.title}</p>
-                  <p>{movie.vote_average}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className="movieList__wrapper">
-          <h2 className="movieList__heading">Weekly Top Rated Movies</h2>
-          <ul className="movieList__list">
-            {this.props.topMovie.map((movie, i) => {
-              if (i > 3) {
-                return;
-              }
-              return (
-                <li key={movie.id} className="movieList__item">
-                  <div>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                      alt="avatar"
-                    />
-                  </div>
-                  <p>{movie.title}</p>
-                  <p>{movie.vote_average}</p>
-                </li>
-              );
-            })}
-          </ul>
-
-          <h2 className="movieList__heading">
-            Weekly Top Rated Bollywood Movies
-          </h2>
-          <ul className="movieList__list">
-            {this.props.bestMovie.map((movie, i) => {
-              if (i > 3) {
-                return;
-              }
-              return (
-                <li key={movie.id} className="movieList__item">
-                  <div>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                      alt="avatar"
-                    />
-                  </div>
-                  <p>{movie.title}</p>
-                  <p>{movie.vote_average}</p>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <ul className="movieList__list">
+          {this.props.searchMovie.map((movie, i) => {
+            return <MovieItem movie={movie} />;
+          })}
+        </ul>
+        {this.props.searchMovie.length === 0 && (
+          <div>
+            <h2 className="movieList__heading">topMovie</h2>
+            <ul className="movieList__list">
+              {this.props.topMovie.map((movie, i) => {
+                if (i > 3) {
+                  return;
+                }
+                return <MovieItem movie={movie} />;
+              })}
+            </ul>
+            <h2 className="movieList__heading">bestMovie</h2>
+            <ul className="movieList__list">
+              {this.props.bestMovie.map((movie, i) => {
+                if (i > 3) {
+                  return;
+                }
+                return <MovieItem movie={movie} />;
+              })}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
