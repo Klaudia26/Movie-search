123b739970e499dceb91e03d98c892a4801de624
Klaudia <klaudia.krzywda@gmail.com>
2020-04-13 12:56:28 +0100

    add slider component

---
+++ src/Slider/Slider.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import MovieItem from '../MovieItem/MovieItem';
+import './Slider.scss';
+
+const Slider = (props) => {
+  return (
+    <div className="slider">
+      <h2 className="slider__heading">{props.heading}</h2>
+      <ul className="slider__list">
+        {props.movies.map((movie) => {
+          return (
+            <MovieItem
+              className="slide"
+              movie={movie}
+              key={movie.id}
+              handleClick={props.handleClick}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Slider;
